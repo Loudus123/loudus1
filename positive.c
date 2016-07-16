c48ed2fcbd522cd4004e1daa240d2ad7cab050ba
Loudus123 <loudusraj@gmail.com>
2016-07-16 14:59:58 +0530

    finish

---
+++ positive.c
@@ -0,0 +1,13 @@
+# loudus1
+#include<stdio.h>
+void main()
+{
+int n=5;
+if(n!=0)
+{
+else if(n>0)
+printf("the number is positive");
+else("the number is negative");
+}
+printf("" the number is zero");
+}
